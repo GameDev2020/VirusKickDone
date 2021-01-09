4fcf3ae407c91291ad8d8c68351fa4d6e3e548ed
rinatoz <rinat1133@gmail.com>
2021-01-09 02:19:38 +0200

    added changes

--- Assets/CenterCollScript.cs
+++ Assets/CenterCollScript.cs
@@ -7,6 +7,10 @@ public class CenterCollScript : MonoBehaviour
     public GameManager gm;
     private Transform previousPlatform;
     public string tag;
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] GameObject[] livesObj;
+    [SerializeField] int life;
+    bool Flife = true;
 
 
     // Start is called before the first frame update
@@ -18,10 +22,23 @@ public class CenterCollScript : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
+      //  if (other.transform.tag == tag && Flife)
+      //  {
+       //     takeDamage();
+      //  }
+      // else
         if (other.transform.tag == tag)
         {
             gm.ResetLevel();
         }
+    }
+
+    public void takeDamage()
+    {
+        this.life = this.life - 1;
+        //livesObj[life].gameObject.SetActive(false);
+        if (life < 1)
+            Flife = false;
     }
 
 
